3359af9c25152077f712031b6bd6bd147cedd561
justin <justinng@Justins-MacBook-Air.local>
2017-11-08 20:42:06 +0800

    implement new action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,26 @@
 class JobsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy ]
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_path
+    end
+  end
+
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
 end
