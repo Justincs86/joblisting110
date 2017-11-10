29b5ccf1f821a6d4dab790f55edaba6726a054fb
justin <justinng@Justins-MacBook-Air.local>
2017-11-10 15:06:41 +0800

    implement wage part

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,8 @@
 class Job < ApplicationRecord
   validates :title, :description, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0 }
+  validates :wage_upper_bound, presence: true
+  validates :contact_email, presence: true
 
   def admin?
     is_admin
