8ec1eb8c404bdad1c263992e6aa23e41a137e0a6
justin <justinng@Justins-MacBook-Air.local>
2017-11-13 13:39:09 +0800

    admin can see resume list

---
+++ app/controllers/admin/resumes_controller.rb
@@ -0,0 +1,20 @@
+class Admin::ResumesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_is_admin
+
+  layout 'admin'
+
+  def index
+    @job = Job.find(params[:job_id])
+    @resumes = @job.resumes.order("created_at DESC")
+  end
+
+ def show
+   @job = Job.find(params[:job_id])
+   @resume = Resume.find(params[:id])
+   @resume.job = @resume
+   @re
+
+ end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,15 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
+
     resources :jobs do
         member do
           post :hide
           post :publish
         end
+
+      resources :resumes
+      
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
